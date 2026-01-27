698913dccf208680c8c66870fccbdf6182ecabb9
ayomanwhathuhhuh <123750756+ayomanwhathuhhuh@users.noreply.github.com>
2026-01-27 15:34:25 -0500

    civadd civadded

--- src/main/java/net/haven/CivAdd/CivAdd.java
+++ src/main/java/net/haven/CivAdd/CivAdd.java
@@ -34,7 +34,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 @Mod(CivAdd.MODID)
 public class CivAdd {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "civadd";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -55,7 +55,7 @@ public class CivAdd {
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .title(Component.translatable("itemGroup.civadd")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
