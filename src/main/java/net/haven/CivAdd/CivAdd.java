0aa32f76ea309bf50d6f7568244bfec2fd756af1
ayomanwhathuhhuh <123750756+ayomanwhathuhhuh@users.noreply.github.com>
2026-01-27 15:41:52 -0500

    new branch

--- src/main/java/net/haven/CivAdd/CivAdd.java
+++ src/main/java/net/haven/CivAdd/CivAdd.java
@@ -29,7 +29,7 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
+// hey man new branch
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CivAdd.MODID)
 public class CivAdd {
